e050929a4c71dc892a5a06aab6bdeb0b3ee89bea
Chris Carlozzi <chris@lozzi.me>
2019-01-10 15:32:44 -0800

    First stab

--- src/slides/Slide4.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import Slide from '../Slide';
-
-export default () => (
-  <Slide>
-    # Slide 4
-  </Slide>
-);
--- src/slides/index.tsx
+++ src/slides/index.tsx
@@ -1,8 +1,50 @@
-import Slide1 from './Slide1';
-import Slide2 from './Slide2';
-import Slide3 from './Slide3';
-import Slide4 from './Slide4';
+import chrisSlide1 from './chrisSlide1';
+import chrisSlide10 from './chrisSlide10';
+import chrisSlide11 from './chrisSlide11';
+import chrisSlide12 from './chrisSlide12';
+import chrisSlide13 from './chrisSlide13';
+import chrisSlide14 from './chrisSlide14';
+import chrisSlide15 from './chrisSlide15';
+import chrisSlide16 from './chrisSlide16';
+import chrisSlide17 from './chrisSlide17';
+import chrisSlide18 from './chrisSlide18';
+import chrisSlide19 from './chrisSlide19';
+import chrisSlide2 from './chrisSlide2';
+import chrisSlide20 from './chrisSlide20';
+import chrisSlide21 from './chrisSlide21';
+import chrisSlide22 from './chrisSlide22';
+import chrisSlide23 from './chrisSlide23';
+import chrisSlide3 from './chrisSlide3';
+import chrisSlide4 from './chrisSlide4';
+import chrisSlide5 from './chrisSlide5';
+import chrisSlide6 from './chrisSlide6';
+import chrisSlide7 from './chrisSlide7';
+import chrisSlide8 from './chrisSlide8';
+import chrisSlide9 from './chrisSlide9';
 
-const slides = [Slide1, Slide2, Slide3, Slide4];
+const slides = [chrisSlide1,
+    chrisSlide2,
+    chrisSlide3,
+    chrisSlide4,
+    chrisSlide5,
+    chrisSlide6, 
+    chrisSlide7,
+    chrisSlide8,
+    chrisSlide9,
+    chrisSlide10,
+    chrisSlide11,
+    chrisSlide12,
+    chrisSlide13,
+    chrisSlide14, 
+    chrisSlide15,
+    chrisSlide16,
+    chrisSlide17,
+    chrisSlide18, 
+    chrisSlide19,
+    chrisSlide20,
+    chrisSlide21,
+    chrisSlide22,
+    chrisSlide23,
+];
 
 export { slides };
